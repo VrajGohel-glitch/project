b71cb61fbf79695abdf8aef88ecc6f967186ed79
Vraj Gohel <vrajgohel2303@gmail.com>
2026-01-02 22:54:53 +0530

    jason file adder

---
+++ test.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Helpdesk Priority Automation',
+    'version': '1.0',
+    'category': 'Helpdesk',
+    'depends': ['base'],
+    'data': [
+        'views/ticket_view.xml',
+    ],
+    'application': True,
+}
